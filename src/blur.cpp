6669671f1c7f0346d05644e9491e538fefa5842d
Xarblu <xarblu@protonmail.com>
2025-11-29 13:19:13 +0100

    Move bbdx exclusive functions to separate file
    Signed-off-by: Xarblu <xarblu@protonmail.com>

--- src/blur.cpp
+++ src/blur.cpp
@@ -443,32 +443,6 @@ void BlurEffect::slotWindowAdded(EffectWindow *w)
     updateBlurRegion(w);
 }
 
-void BlurEffect::slotWindowClosed(EffectWindow *w)
-{
-    /*
-     * Some windows (e.g. the foot terminal) like to
-     * close by simply deleting themselves.
-     * This causes e.g. the "scale" window close effect
-     * to blur and apply contrast to a fully transparent area.
-     *
-     * With modified contrast/saturation this at best looks bad
-     * and at worst causes a blinding flash with bright background surfaces.
-     * Clamp them to mostly work around the issue.
-     */
-    if (w && w->isDeleted()) {
-        BlurEffectData &data = m_windows[w];
-
-        const qreal brightness = data.brightness.value_or(m_settings.general.brightness);
-        data.brightness = std::min(brightness, 1.0);
-
-        const qreal saturation = data.saturation.value_or(m_settings.general.saturation);
-        data.saturation = std::min(saturation, 1.0);
-
-        const qreal contrast = data.contrast.value_or(m_settings.general.contrast);
-        data.contrast = std::min(contrast, 1.0);
-    }
-}
-
 void BlurEffect::slotWindowDeleted(EffectWindow *w)
 {
     if (auto it = m_windows.find(w); it != m_windows.end()) {
@@ -707,25 +681,6 @@ bool BlurEffect::shouldBlur(const EffectWindow *w, int mask, const WindowPaintDa
     }
 
     return hasForceBlurRole;
-}
-
-bool BlurEffect::shouldForceBlur(const EffectWindow *w) const
-{
-    const auto windowClass = w->window()->resourceClass();
-    const auto layer = w->window()->layer();
-    if (w->isDesktop()
-        || (!m_settings.forceBlur.blurDocks && w->isDock())
-        || (!m_settings.forceBlur.blurMenus && isMenu(w))
-        || windowClass == "xwaylandvideobridge"
-        || ((windowClass == "spectacle" || windowClass == "org.kde.spectacle")
-            && (layer == Layer::OverlayLayer || layer == Layer::ActiveLayer))) {
-        return false;
-    }
-
-    bool matches = m_settings.forceBlur.windowClasses.contains(w->window()->resourceName())
-        || m_settings.forceBlur.windowClasses.contains(w->window()->resourceClass());
-    return (matches && m_settings.forceBlur.windowClassMatchingMode == WindowClassMatchingMode::Whitelist)
-        || (!matches && m_settings.forceBlur.windowClassMatchingMode == WindowClassMatchingMode::Blacklist);
 }
 
 void BlurEffect::drawWindow(const RenderTarget &renderTarget, const RenderViewport &viewport, EffectWindow *w, int mask, const QRegion &region, WindowPaintData &data)
---
+++ src/blur_extensions.cpp
@@ -0,0 +1,60 @@
+/*
+ * File containing functions only present in
+ * Better Blur DX
+ */
+
+
+#include "blur.h"
+#include "utils.h"
+
+#include <effect/effectwindow.h>
+
+namespace KWin
+{
+
+void BlurEffect::slotWindowClosed(EffectWindow *w)
+{
+    /*
+     * Some windows (e.g. the foot terminal) like to
+     * close by simply deleting themselves.
+     * This causes e.g. the "scale" window close effect
+     * to blur and apply contrast to a fully transparent area.
+     *
+     * With modified contrast/saturation this at best looks bad
+     * and at worst causes a blinding flash with bright background surfaces.
+     * Clamp them to mostly work around the issue.
+     */
+    if (w && w->isDeleted()) {
+        BlurEffectData &data = m_windows[w];
+
+        const qreal brightness = data.brightness.value_or(m_settings.general.brightness);
+        data.brightness = std::min(brightness, 1.0);
+
+        const qreal saturation = data.saturation.value_or(m_settings.general.saturation);
+        data.saturation = std::min(saturation, 1.0);
+
+        const qreal contrast = data.contrast.value_or(m_settings.general.contrast);
+        data.contrast = std::min(contrast, 1.0);
+    }
+}
+
+bool BlurEffect::shouldForceBlur(const EffectWindow *w) const
+{
+    const auto windowClass = w->window()->resourceClass();
+    const auto layer = w->window()->layer();
+    if (w->isDesktop()
+        || (!m_settings.forceBlur.blurDocks && w->isDock())
+        || (!m_settings.forceBlur.blurMenus && isMenu(w))
+        || windowClass == "xwaylandvideobridge"
+        || ((windowClass == "spectacle" || windowClass == "org.kde.spectacle")
+            && (layer == Layer::OverlayLayer || layer == Layer::ActiveLayer))) {
+        return false;
+    }
+
+    bool matches = m_settings.forceBlur.windowClasses.contains(w->window()->resourceName())
+        || m_settings.forceBlur.windowClasses.contains(w->window()->resourceClass());
+    return (matches && m_settings.forceBlur.windowClassMatchingMode == WindowClassMatchingMode::Whitelist)
+        || (!matches && m_settings.forceBlur.windowClassMatchingMode == WindowClassMatchingMode::Blacklist);
+}
+
+}
